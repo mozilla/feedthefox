40a26bb572d22ea40ae52c563c7e6626e9bc8d98
Tasos Katsoulas <akatsoulas@gmail.com>
2015-10-22 15:33:36 +0300

    Update device model to use m2m with users.

--- feedthefox/devices/admin.py
+++ feedthefox/devices/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
-from feedthefox.devices.models import Build, Device
+from feedthefox.devices.models import Build, Device, DeviceInfo
+
+
+class DeviceInfoInline(admin.TabularInline):
+    model = DeviceInfo
+    extra = 1
 
 
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
+    inlines = (DeviceInfoInline,)
     list_display = ('model', 'manufacturer',)
 
 
