f7b6f010b6e1f25d1f6585b033c60c28b44bfa03
John Giannelos <johngiannelos@gmail.com>
2015-11-05 20:53:49 +0100

    Implement delete device info functionality.

--- feedthefox/users/views.py
+++ feedthefox/users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect, render
 
+from feedthefox.devices.models import DeviceInfo
 from feedthefox.users import forms
 
 
@@ -12,14 +14,13 @@ def view_profile(request):
     device_info_form = forms.UserDeviceInfoForm(request.POST or None,
                                                 prefix='device_info')
 
-    if newsletter_form.is_valid() and 'newsletter' in request.POST:
-        newsletter_form.save()
-        return redirect('view_profile')
-
-    if device_info_form.is_valid() and 'device_info' in request.POST:
+    if 'device_info' in request.POST and device_info_form.is_valid():
         device_info = device_info_form.save(commit=False)
         device_info.user = request.user
         device_info.save()
+        return redirect('view_profile')
+    elif newsletter_form.is_valid():
+        newsletter_form.save()
         return redirect('view_profile')
 
     ctx = {
@@ -28,3 +29,14 @@ def view_profile(request):
     }
 
     return render(request, 'profile.html', ctx)
+
+
+@login_required
+def delete_device_info(request, id):
+    device_info = get_object_or_404(DeviceInfo, pk=id)
+
+    if not device_info.user == request.user:
+        raise Http404
+
+    device_info.delete()
+    return redirect('view_profile')
