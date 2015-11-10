12f63fa25461e2abf8eae0be3e3f6308526adcbb
Nikos Roussos <nikos@roussos.cc>
2015-11-10 10:19:21 +0000

    Add legal text for IMEI (Fixes #100)

--- feedthefox/base/static/js/profile.js
+++ feedthefox/base/static/js/profile.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function ($) {
     'use strict';
 
-    var t = $('#imei-input');
+    var t = $('#id_device_info-imei');
     t.bind('propertychange keyup input paste', function(event) {
         var target = $('.imei-tos');
         var term = t.val();
-        (term !== '' ? $(target).show() : $(target).hide());
+        (term !== '' ? $(target).show('fast') : $(target).hide('fast'));
     });
 
     if ($('#DeviceModal .errorlist').length > 0) {
