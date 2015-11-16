5a69dd5eb9d9e33fc5d8f3cf6a5fb7dba92ae6de
John Giannelos <johngiannelos@gmail.com>
2015-11-16 13:02:21 +0100

    Add revision.txt to keep track of deployed reference.

--- bin/run-prod.sh
+++ bin/run-prod.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 ./bin/run-common.sh
+
+# Update revision.txt with latest HEAD
+echo $(git rev-parse HEAD) > static/revision.txt
+
 gunicorn feedthefox.wsgi:application -b 0.0.0.0:${PORT:-8000} --log-file -
